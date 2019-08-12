e16dcb9c84c679d41f077407b0c5089ae28ba200
ara-mneji <ara.menjivar7489@gmail.com>
2019-08-12 10:00:49 -0400

    js file

---
+++ templates/game_page.js
@@ -0,0 +1,5 @@
+const alphabet = document.querySelector('#alphabet')
+alphabet.addEventListener('click', (event)=> {
+  console.log('letter clicked');
+  alphabet.style.backgroundColor= 'Orange';
+})
