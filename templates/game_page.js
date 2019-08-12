a3b0869cf7f9136e74e1d83c1402f09500258311
ara-mneji <ara.menjivar7489@gmail.com>
2019-08-12 14:48:49 -0400

    highlighted buttons

--- templates/game_page.js
+++ templates/game_page.js
@@ -1,8 +1,10 @@
-const alphabet = document.querySelector('#alphabet')
-document.addEventListener('onclick', (event)=> {
-  console.log('letter clicked');
-  alphabet.style.backgroundColor= 'yellow';
-})
+// const alphabet = document.querySelector('#alphabet')
+// document.addEventListener('onclick', (event)=> {
+//   console.log('letter clicked');
+//   alphabet.style.backgroundColor= 'yellow';
+// })
+
+// single click to highlight, another click to take out highlight
 var highlight = (button) => {
   if(button.style.backgroundColor == 'yellow'){
     console.log('letter clicked');
